3c3137c21c5a61419c419d06e2d3c0939b4f4ad4
Oscar Murrieta <oscarmlmurrieta@gmail.com>
2025-03-03 20:55:45 -0600

    Feature: Set up Snake structure

--- main.py
+++ main.py
@@ -6,11 +6,32 @@ class Window():
         self.width: int = width
         self.height: int = height
         self.name: str = name
-    
-window = Window(1280, 720, "Snake Game")
+        
+class Snake():
+    def __init__(self) -> None:
+        self.pos: List[int] = [0, 0]
+        self.vel: List[int] = [1, 3]
+        self.width: int = 30
+        self.height: int = 30
+        self.color: tuple[int] = (75, 100, 225, 250)
+            
+window = Window(720, 720, "Snake Game")
 pr.init_window(window.width, window.height, window.name)
+pr.set_target_fps(160)
+
+snake = Snake()
 
 while not pr.window_should_close():
+    snake.pos[0] += snake.vel[0]
+
+    
+    if snake.pos[0] > window.width - snake.width:
+            snake.pos[0] = -snake.width -1
+           
     pr.begin_drawing()
+    pr.clear_background(pr.BLACK)
+    pr.draw_rectangle(snake.pos[0], snake.pos[1],
+                      snake.width, snake.height, snake.color)
+
     pr.end_drawing()
 pr.close_window()
